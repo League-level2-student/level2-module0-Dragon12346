da2e722529e753d769cb9845d49cbd05ab7429a3
audreyizadi <league@iMac-2.attlocal.net>
2019-01-11 18:28:16 -0800

    RobotRace,FindTheHiddenButton(IP)

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,22 +1,68 @@
 package arrays;
 
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class _01_RobotRace {
-	//1. make a main method
 
-	//2. create an array of 5 robots.
+	// 1. make a main method
+	public static void main(String[] args) {
+		 Robot[] rob = new Robot[5];
 
-	//3. use a for loop to initialize the robots.
 
-		//4. make each robot start at the bottom of the screen, side by side, facing up
 
-	//5. use another for loop to iterate through the array and make each robot move 
-	//   a random amount less than 50.
+					
+		for (int i = 0; i < rob.length; i++) {
+			Robot r = rob[i] = new Robot();
 
-	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
+			r.setX(i * 100 + 200);
+		}
+		int i = 0;
+		for (int j = 0; j < 1000; j++) {
+			rob[i].penDown();
+			rob[i].setPenWidth(43);
+		 int rand2 = new Random().nextInt(256);
+		 int rand3 = new Random().nextInt(256);
+		 int rand4 = new Random().nextInt(256);
+		 rob[i].setPenColor(rand4,rand3,rand2);
+			rob[i].setSpeed(10);
+			int rand = new Random().nextInt(50);
+			rob[i].move(rand);
 
-	//7. declare that robot the winner and throw it a party!
+			int y = rob[i].getY();
+			System.out.println(y);
+			if (y < 0) {
+				JOptionPane.showMessageDialog(null, "Robot " + i + " has won!");
+				j += 100000;
+			}
+			i++;
+			if (i == 5) {
+				i = 0;
+			}
+		}
 
-	//8. try different races with different amounts of robots.
+	
+	
+	
+	
+	
+	}
+	// 2. create an array of 5 robots.
 
-	//9. make the robots race around a circular track.
+
+	// 3. use a for loop to initialize the robots.
+
+	// 4. make each robot start at the bottom of the screen, side by side, facing up
+
+	// 5. use another for loop to iterate through the array and make each robot move
+	// a random amount less than 50.
+
+	// 6. use a while loop to repeat step 5 until a robot has reached the top of the
+	// screen.
+
+	// 7. declare that robot the winner and throw it a party!
+
 }
--- src/arrays/_02_FindTheHiddenButton.java
+++ src/arrays/_02_FindTheHiddenButton.java
@@ -18,9 +18,9 @@ import javax.swing.JPanel;
 public class _02_FindTheHiddenButton implements ActionListener{
 	JFrame window;
 	JPanel panel;
-	
+	JButton []buttons;
 	//1. create an array of JButtons. Don't initialize it yet.
-	
+	int hiddenButton;
 	//2 create an int variable called hiddenButton
 	
 	public static void main(String[] args) {
@@ -33,7 +33,8 @@ public class _02_FindTheHiddenButton implements ActionListener{
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		//3.  Ask the user to enter a positive number and convert it to an int
-		
+		String num = JOptionPane.showInputDialog("Enter a positive number");
+		int an = Integer.parseInt(num);
 		//4. Initialize the array of JButtons to be the size of the int created in step 3
 		
 		//5. Make a for loop to iterate through the JButton array
